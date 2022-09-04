f2655e0172d9ce8f29551a68e77114fb88b0edf5
Andrii Laptiiev <andreylaptiev13@gmail.com>
2022-09-04 17:35:18 +0300

    Add buttons to start game

--- GuessTheTartget/Views/MainView.swift
+++ GuessTheTartget/Views/MainView.swift
@@ -15,6 +15,29 @@ class MainView: UIView {
         return label
     }()
 
+    static let buttonConfiguration: UIButton.Configuration = {
+        var configuration = UIButton.Configuration.gray()
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 10.0, leading: 15.0, bottom: 10.0, trailing: 15.0)
+        configuration.cornerStyle = .capsule
+        return configuration
+    }()
+
+    let guessTheNumberButton: UIButton = {
+        let button = UIButton(configuration: MainView.buttonConfiguration, primaryAction: nil)
+        button.setTitle("Guess the number", for: .normal)
+        button.tintColor = .purple
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let guessTheColorButton: UIButton = {
+        let button = UIButton(configuration: MainView.buttonConfiguration, primaryAction: nil)
+        button.setTitle("Guess the color", for: .normal)
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,16 +55,26 @@ class MainView: UIView {
     }
 
     func setupView() {
-        layer.backgroundColor = UIColor.red.cgColor
+        layer.backgroundColor = UIColor.white.cgColor
         translatesAutoresizingMaskIntoConstraints = false
 
         addSubview(titleLabel)
+        addSubview(guessTheNumberButton)
+        addSubview(guessTheColorButton)
     }
+}
 
+extension MainView {
     func setConstraints() {
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            guessTheNumberButton.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -20),
+            guessTheNumberButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            guessTheColorButton.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 20),
+            guessTheColorButton.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
     }
 }
